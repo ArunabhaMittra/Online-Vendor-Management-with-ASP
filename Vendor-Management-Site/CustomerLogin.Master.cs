a8acafe99ed2487d2e51cd714c1f40599e83b3f6
IndyMacTavish <arunabha.mitra97@outlook.com>
2020-07-02 13:04:39 +0530

    Added Customer Dashboard Master and Service Page

---
+++ Vendor-Management-Site/CustomerLogin.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Vendor_Management_Site
+{
+    public partial class CustomerLogin : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
